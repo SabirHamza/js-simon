d3521e675572e8942c85965df22d664235273ca0
SabirHamza <h.sabir.ma@gmail.com>
2023-02-06 16:46:02 +0100

    countdown

--- js/main.js
+++ js/main.js
@@ -2,24 +2,31 @@ const secondEl = document.getElementById("second");
 const minuteEl = document.getElementById("minute");
 const hourEl = document.getElementById("hour");
 
-const now = new Date();
+const now = new Date("2023-02-06 09:30:00");
 console.log(now);
 
-const tomorrow = new Date ("2023-02-06 09:30:00");
+const tomorrow = new Date ("2023-02-07 09:30:00");
 console.log(tomorrow);
 
-let timeleft = tomorrow - now;
+const sTomorrow = tomorrow.getTime()
+console.log(sTomorrow);
+const snow = now.getTime()
+console.log(snow);
+
+let timeleft = sTomorrow - snow;
 console.log(timeleft);
 
 const clock = setInterval(countdown, 1000)
 
 function countdown() {
 
+    //let timing = Math.floor(timeleft / 1000)
+
     --timeleft;
 
     const second = (timeleft % 60);
-    const minute = Math.floor((timeleft / 60) % 60);
-    const hour = Math.floor((timeleft / 60 / 60) % 24);
+    const minute = Math.floor((timeleft / 60)) % 60;
+    const hour = Math.floor((timeleft / 60 / 60)) % 24;
 
     secondEl.innerHTML = (second < 10) ? "0" + second : second;
     minuteEl.innerHTML = (minute < 10) ? "0" + minute + ":" : minute + ":";
